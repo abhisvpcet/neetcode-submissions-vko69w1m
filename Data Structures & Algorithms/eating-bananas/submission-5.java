bd1971eee597c92e26b9bd160a52321a4ccc40dd
abhisvpcet <103098612+abhisvpcet@users.noreply.github.com>
2026-04-04 19:52:48 +0530

    Add: eating-bananas - submission-5

---
+++ Data Structures & Algorithms/eating-bananas/submission-5.java
@@ -0,0 +1,39 @@
+class Solution {
+    public int minEatingSpeed(int[] piles, int h) {
+
+       int start=1;
+       int end= maxElement(piles);
+       int ans= end;
+
+       while(start<=end){
+        int speed= (start+end)/2;
+
+        int noOfHours=calculateNoOfHours(piles, speed);
+        if(noOfHours>h){
+            start=speed+1;
+        }
+        else{
+            ans= speed;
+            end= speed-1;
+        }
+       }
+       return ans;
+    }
+    private int maxElement(int[] piles){
+        int maxEle= piles[0];
+        for(int num: piles){
+            if(num>maxEle){
+                maxEle= num;
+            }
+        }
+        return maxEle;
+    }
+
+    private int calculateNoOfHours(int[] piles, int speed){
+        int noOfHours=0;
+        for(int pile: piles){
+            noOfHours +=Math.ceil((pile+0.0)/speed);
+        }
+        return noOfHours;
+    }
+}
