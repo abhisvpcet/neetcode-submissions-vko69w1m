a546c194d545a1538e1e9d34474934835cee0c40
abhisvpcet <103098612+abhisvpcet@users.noreply.github.com>
2026-04-05 08:12:17 +0530

    Add: merge-two-sorted-linked-lists - submission-4

---
+++ Data Structures & Algorithms/merge-two-sorted-linked-lists/submission-4.java
@@ -0,0 +1,54 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+
+class Solution {
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+
+        ListNode fp= list1;
+        ListNode sp= list2;
+        ListNode nodeToBeCopied=null;
+        ListNode head= null, tail= null;
+
+        while(fp!=null || sp!=null){
+            if(fp!=null && sp!=null){
+                if(fp.val<=sp.val){
+                    nodeToBeCopied= fp;
+                    fp=fp.next;
+                }
+                else{
+                    nodeToBeCopied= sp;
+                    sp=sp.next;
+                }
+            }
+            else if(fp!=null){
+                nodeToBeCopied= fp;
+                fp= fp.next;
+            }
+            else{
+                nodeToBeCopied= sp;
+                sp=sp.next;
+            }
+            tail= insertAtEnd(tail, nodeToBeCopied.val);
+                if(head==null){
+                    head=tail;
+                }
+            }
+            return head;
+        }
+
+        private ListNode insertAtEnd(ListNode tail, int val){
+            ListNode nodeToBeInserted= new ListNode(val);
+            if(tail!=null){
+                tail.next= nodeToBeInserted;
+            }
+            return nodeToBeInserted;
+        }
+}
