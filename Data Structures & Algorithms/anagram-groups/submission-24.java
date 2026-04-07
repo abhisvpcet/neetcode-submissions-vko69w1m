dcc33487851ab25ea7a7762904ee3e03086db74f
abhisvpcet <103098612+abhisvpcet@users.noreply.github.com>
2026-04-07 08:26:26 +0530

    Add: anagram-groups - submission-24

---
+++ Data Structures & Algorithms/anagram-groups/submission-24.java
@@ -0,0 +1,21 @@
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+ 
+        Map<String, List<String>> answer= new HashMap<>();
+
+        for(String str: strs){
+            char[] chars= str.toCharArray();
+            Arrays.sort(chars);
+
+            String key= new String(chars);
+
+            if(!answer.containsKey(key)){
+                answer.put(key, new ArrayList<>());
+            }
+            answer.get(key).add(str);
+        }
+        return new ArrayList<>(answer.values());
+
+    }
+}
+
