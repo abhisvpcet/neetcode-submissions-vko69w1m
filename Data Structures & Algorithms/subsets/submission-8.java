ff86e3b39719e00def5a738c810457049327837e
abhisvpcet <103098612+abhisvpcet@users.noreply.github.com>
2026-04-19 16:51:52 +0530

    Add: subsets - submission-8

---
+++ Data Structures & Algorithms/subsets/submission-8.java
@@ -0,0 +1,28 @@
+class Solution {
+
+   public List<List<Integer>>  subsets;
+   public int[] nums;
+
+    public List<List<Integer>> subsets(int[] nums) {
+
+        subsets= new ArrayList<>();
+        this.nums= nums;
+
+        List<Integer> subset= new ArrayList<>();
+        backtrack(0, subset);
+        return new ArrayList<>(subsets);
+     
+    }
+
+    void backtrack(int i, List<Integer> subset){
+        if(i>=nums.length){
+            subsets.add(new ArrayList<>(subset));
+        }
+        else{
+            backtrack(i+1, subset);
+            subset.add(nums[i]);
+            backtrack(i+1, subset);
+            subset.remove(subset.size()-1);
+        }
+    }
+}
