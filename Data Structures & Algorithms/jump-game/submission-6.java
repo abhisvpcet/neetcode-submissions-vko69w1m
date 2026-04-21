5001400a893a2f3bca7d50097287cf7334658edb
abhisvpcet <103098612+abhisvpcet@users.noreply.github.com>
2026-04-21 21:46:12 +0530

    Add: jump-game - submission-6

---
+++ Data Structures & Algorithms/jump-game/submission-6.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean canJump(int[] nums) {
+    
+     int n= nums.length;
+     boolean[] dp= new boolean[n];
+     dp[n-1]= true;
+
+     for(int i=n-2; i>=0; i--){
+        for(int j=1; j<= nums[i] && dp[i]==false && i+1<n; j++){
+            dp[i]= dp[i+j];
+        }
+     }
+     return dp[0];
+    }
+}
