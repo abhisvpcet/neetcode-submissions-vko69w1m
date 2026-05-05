d06adf3d08bb92702974876b9e372a7e3093c4bc
abhisvpcet <103098612+abhisvpcet@users.noreply.github.com>
2026-05-05 08:04:11 +0530

    Add: longest-consecutive-sequence - submission-24

---
+++ Data Structures & Algorithms/longest-consecutive-sequence/submission-24.java
@@ -0,0 +1,32 @@
+class Solution {
+    public int longestConsecutive(int[] nums) {
+
+    int ans=0;
+    Map<Integer, Boolean> answer= new HashMap<>();
+
+    for(int num: nums){
+      answer.put(num, true);
+    }
+
+    for(int num:answer.keySet()){
+        if(answer.containsKey(num-1)){
+            continue;
+        }
+        int nextNum= num+1;
+        int count=1;
+
+        while(answer.containsKey(nextNum)){
+            nextNum++;
+            count++;
+        }
+        if(count>ans){
+            ans= count;
+        }
+    }
+    return ans;
+
+
+
+    }
+}
+
