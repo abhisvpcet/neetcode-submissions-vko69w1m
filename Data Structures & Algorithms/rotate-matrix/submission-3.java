425516db97360f3f74fb12e4d8b3e670c57dd21d
abhisvpcet <103098612+abhisvpcet@users.noreply.github.com>
2026-05-17 13:17:49 +0530

    Add: rotate-matrix - submission-3

---
+++ Data Structures & Algorithms/rotate-matrix/submission-3.java
@@ -0,0 +1,35 @@
+class Solution {
+    public void rotate(int[][] matrix) {
+
+       //to find transpose;
+
+       int n= matrix.length;
+
+       for(int i=0; i<n; i++){
+        for(int j=i; j<n; j++){
+
+            int temp= matrix[i][j];
+            matrix[i][j]= matrix[j][i];
+            matrix[j][i]= temp;
+        }
+       }
+
+       //reverse
+
+       for(int row=0; row<n; row++){
+        int left= 0;
+        int right=n-1;
+
+        while(left<right){
+            int temp= matrix[row][left];
+            matrix[row][left]= matrix[row][right];
+            matrix[row][right]= temp;
+
+            left++;
+            right--;
+            
+                    }
+       }
+        
+    }
+}
