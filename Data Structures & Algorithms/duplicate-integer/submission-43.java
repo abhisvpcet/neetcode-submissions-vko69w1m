177723e2bd99890879be3e7eb76637d64dc47a0c
abhisvpcet <103098612+abhisvpcet@users.noreply.github.com>
2026-09-20 21:21:45 +0530

    Add: duplicate-integer - submission-43

---
+++ Data Structures & Algorithms/duplicate-integer/submission-43.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean hasDuplicate(int[] nums) {
+
+      HashSet<Integer> set= new HashSet<>();
+
+      for(int num: nums){
+        while(!set.add(num)){
+            return true;
+        }
+      
+      }
+        return false;
+        
+    }
+}
